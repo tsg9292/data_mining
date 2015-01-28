b1e72d64f9e6ce6e313d95051cf23f2c9c6a22c9
Taylor <tsg9292@gmail.com>
2015-01-28 11:27:19 -0700

    reading single posts from twitter.  Need to figure out how to handle the json result.

--- a1/twitter_reader.py
+++ a1/twitter_reader.py
@@ -1,4 +1,6 @@
+import sys
 import requests
+import json
 from requests_oauthlib import OAuth1
 from urlparse import parse_qs
 
@@ -6,11 +8,11 @@ REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
 AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize?oauth_token="
 ACCESS_TOKEN_URL = "https://api.twitter.com/oauth/access_token"
 
-CONSUMER_KEY = "YOUR CONSUMER KEY"
-CONSUMER_SECRET = "YOUR CONSUMER SECRET"
+CONSUMER_KEY = "FvHpVtiweeZ1Db4Q89AtXYmHE"
+CONSUMER_SECRET = "vMZ0Ek28yv3jcnjVh4GDwMvAwPbyuT0VsA5qJNAYxDzP8iirnX"
 
-OAUTH_TOKEN = ""
-OAUTH_TOKEN_SECRET = ""
+OAUTH_TOKEN = "156509990-2OENzM4s6crQlUI9V6geS54tGEN0pEQVQ7Pvb2Xr"
+OAUTH_TOKEN_SECRET = "szk6SWcmyRSSW5ECQRKyFyFkERj62cU1WRdydPSdMP8EU"
 
 
 def setup_oauth():
@@ -43,9 +45,13 @@ def setup_oauth():
 	return token, secret
 
 def get_oauth():
-	print "getting oauth"
+	oauth = OAuth1(CONSUMER_KEY,
+				   client_secret=CONSUMER_SECRET,
+				   resource_owner_key=OAUTH_TOKEN,
+				   resource_owner_secret=OAUTH_TOKEN_SECRET)
+	return oauth
 
-def main():
+if __name__ == "__main__":
 	if not OAUTH_TOKEN:
 		token, secret = setup_oauth()
 		print "OAUTH_TOKEN: "+token
@@ -53,8 +59,10 @@ def main():
 		print
 	else:
 		oauth = get_oauth()
-		r = requests.get(url="https://api.twitter.com/1.1/statuses/mentions_timeline.json", auth=oauth)
-		print r.json()
-
-if __name__ == "__main__":
-	main()
+		r = requests.get(url="https://api.twitter.com/1.1/statuses/user_timeline.json?count=1", auth=oauth)
+		result=str(r.json())
+		result = json.dumps(result,indent=1)
+		f = open('results.json', 'w')
+		f.write(result)
+		f.close()
+		sys.exit(0)
